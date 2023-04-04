9222e6a5dae04d8125d5fc632a342296c9046e35
eliott <eliott.jaquier@gmail.com>
2023-04-04 22:57:54 +0200

    AudioMotor optinonal tires

--- SoundManager/Utils/AudioMotor.cs
+++ SoundManager/Utils/AudioMotor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 namespace SoundManager
 {
     [RequireComponent(typeof(AudioSource))]
+    [RequireComponent(typeof(AudioLowPassFilter))]
     public class AudioMotor : MonoBehaviour
     {
         public AudioClip motorClip;
@@ -16,9 +17,14 @@ namespace SoundManager
         public float motorThrustChangeReactivity = 10f;
 
         [SerializeField]
+        AudioSource optionnalTiresOnRoad;
+
+        [SerializeField]
         private bool startEngineOnAwake = true;
 
         private AudioSource audioSource;
+        private AudioLowPassFilter lowPassFilter;
+        private float actualTargetSpeed;
         private float targetPitch;
         private float targetThrust;
         private bool isEngineStarted = false;
@@ -27,6 +33,7 @@ namespace SoundManager
         {
             audioSource = GetComponent<AudioSource>();
             audioSource.playOnAwake = false;
+            lowPassFilter = GetComponent<AudioLowPassFilter>();
             if (startEngineOnAwake)
             {
                 StartEngine();
@@ -38,11 +45,23 @@ namespace SoundManager
             if (targetPitch != audioSource.pitch)
             {
                 audioSource.pitch = Mathf.Lerp(audioSource.pitch, targetPitch, Time.deltaTime * motorPitchChangeReactivity);
+                if (optionnalTiresOnRoad != null)
+                {
+                    optionnalTiresOnRoad.volume = actualTargetSpeed;
+                }
             }
             if (targetThrust != audioSource.volume)
             {
                 audioSource.volume = Mathf.Lerp(audioSource.volume, targetThrust, Time.deltaTime * motorThrustChangeReactivity);
-                if (audioSource.volume == 0) audioSource.Stop();
+                lowPassFilter.cutoffFrequency = Mathf.Lerp(7000, 14000, audioSource.volume);
+                if (audioSource.volume == 0)
+                {
+                    audioSource.Stop();
+                    if (optionnalTiresOnRoad != null)
+                    {
+                        optionnalTiresOnRoad.Stop();
+                    }
+                }
             }
         }
 
@@ -59,6 +78,11 @@ namespace SoundManager
             audioSource.clip = motorClip;
             audioSource.loop = true;
             audioSource.Play();
+            if(optionnalTiresOnRoad != null)
+            {
+                optionnalTiresOnRoad.loop = true;
+                optionnalTiresOnRoad.Play();
+            }
             isEngineStarted = true;
         }
 
@@ -68,6 +92,7 @@ namespace SoundManager
         public void StopEngine()
         {
             if (!isEngineStarted) return;
+            actualTargetSpeed = 0;
             targetPitch = motorMinimumPitch;
             targetThrust = 0;
         }
@@ -79,6 +104,7 @@ namespace SoundManager
         public void SetTargetSpeed(float speedPercentage)
         {
             if (!isEngineStarted) return;
+            actualTargetSpeed = speedPercentage;
             targetPitch = Mathf.Lerp(motorIdlePitch, motorMaxPitch, speedPercentage);
         }
 
